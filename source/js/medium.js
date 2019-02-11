fe26eda60ab657e8f71de98edf0fff4031c3a128
Geno Roupsky <geno@roupsky.name>
2019-02-11 21:59:24 +0200

    Sample static site

---
+++ source/js/medium.js
@@ -0,0 +1,11 @@
+MediumWidget.Init({
+  renderTo: '#medium-widget',
+  params: {
+    'resource': 'https://uxdesign.cc',
+    'postsPerLine': 2,
+    'limit': 4,
+    'picture': 'big',
+    'fields': ['description', 'author', 'claps', 'publishAt'],
+    'ratio': 'landscape'
+  }
+})
